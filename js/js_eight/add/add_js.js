c06487cb9b4b1f2f7ec9cdf034140cea9af2a23e
Stegcorp <66737889+Stegcorp@users.noreply.github.com>
2022-02-21 09:32:14 +0100

    js eight

--- js/js_eight/add/add_js.js
+++ js/js_eight/add/add_js.js
@@ -1,14 +1,21 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в
 // окремий масив. масив вивести в консоль
-//     let array = []
-// function classes(enterElement) {
-//     // console.log(enterElement);
-//     let ars =enterElement.getAttribute('class')
-// if (enterElement.getAttribute('class'))array.push(ars)
-//     let children = enterElement.children;
-//     for (const child of children) {
-//         classes(child)
-//     }
-// }
-// classes(document.body)
-// console.log(array)
+    let array = []
+function classes(enterElement) {
+    // console.log(enterElement);
+    let ars =enterElement.getAttribute('class')
+if (enterElement.getAttribute('class'))array.push(ars)
+    let children = enterElement.children;
+    for (const child of children) {
+        classes(child)
+    }
+}
+classes(document.body)
+let arr = []
+for (const arrayElement of array) {
+ let aa =  arrayElement.split(' ')
+    for (const aaElement of aa) {
+        arr.push(aaElement)
+    }
+}
+console.log(arr)
